6ab632abf8c1d12d827d4146d6f3b95c5fdc33fe
fenqing <1286976336@qq.com>
2020-06-29 01:44:50 +0800

    项目结构

--- user-producer/src/main/java/com/glsc/UserProducerApplication.java
+++ user-producer/src/main/java/com/glsc/UserProducerApplication.java
@@ -1,11 +1,13 @@
 package com.glsc;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubboConfiguration
+@EnableDubboConfig
+@DubboComponentScan("com.glsc.model.**.impl")
 public class UserProducerApplication {
 
     public static void main(String[] args) {
