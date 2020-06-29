63560eee01a01a56170cece79e4cbf4b5ddf43f3
YinMingBin <2753354663@qq.com>
2020-06-29 13:43:08 +0800

    .

---
+++ user-consumer/src/main/java/com/glsc/UserConsumerApplication.java
@@ -0,0 +1,17 @@
+package com.glsc;
+
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableDubboConfig
+//@DubboComponentScan("com.glsc.model.**.impl")
+public class UserProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProducerApplication.class, args);
+    }
+
+}
---
+++ user-consumer/src/main/java/com/glsc/model/user/controller/UserController.java
@@ -0,0 +1,41 @@
+package glsc.model.user.controller;
+
+import glsc.model.user.bean.pojo.User;
+import glsc.model.user.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Description: TODO
+ * @author: scott
+ * @date: 2020年06月27日 18:00
+ */
+@RestController
+public class UserController {
+
+    @Autowired
+    private IUserService userService;
+
+    @GetMapping("/users")
+    public List<User> getUser(){
+        return userService.getUser();
+    }
+
+    @PostMapping("/user")
+    public boolean addUser(@RequestBody User user){
+        return userService.addUser(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public boolean delete(@PathVariable("id") Integer id){
+        return userService.delete(id);
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user){
+        return userService.updateUser(user);
+    }
+
+}
