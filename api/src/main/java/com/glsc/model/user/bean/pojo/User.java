f8e53be225a10b6ef4270adea615f93952643b7a
fenqing <1286976336@qq.com>
2020-06-29 23:48:51 +0800

    .

--- api/src/main/java/com/glsc/model/user/bean/pojo/User.java
+++ api/src/main/java/com/glsc/model/user/bean/pojo/User.java
@@ -2,13 +2,15 @@ package com.glsc.model.user.bean.pojo;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @Description: TODO
  * @author: scott
  * @date: 2020年06月27日 17:50
  */
 @Data
-public class User {
+public class User implements Serializable {
     private Integer id;
     private String username;
     private String password;
